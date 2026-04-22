ca44c3022b0d48df7f35a9563a2eb815b927d3fe
chandraprakashmishra18 <prashantmishra44140@gmail.com>
2026-04-22 14:23:09 +0530

    okkk

--- R-project/src/components/Hollywood.jsx
+++ R-project/src/components/Hollywood.jsx
@@ -1,20 +1,8 @@
 import React from 'react'
 
 const Hollywood = () => {
-  const HollywoodData = Data.filter(
-    (item) => item.category === "Hollywood"
-  );
-  console.log(HollywoodData)
   return (
     <div>
-      {HollywoodData.map((hollydata , index) => (
-        <div key = {index}>
-          <h1>
-            {hollydata.title}
-          </h1>
-          <img src={hollydata.img_url} alt={hollydata.title}/>
-          </div>
-      ))}
       
     </div>
   )
