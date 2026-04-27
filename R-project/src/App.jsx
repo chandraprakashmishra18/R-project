170afac7d35fd411052d7c27e32011aacf46f2df
chandraprakashmishra18 <prashantmishra44140@gmail.com>
2026-04-27 15:11:04 +0530

    okko

--- R-project/src/App.jsx
+++ R-project/src/App.jsx
@@ -8,8 +8,10 @@ import Fitness from "./components/Fitness";
 import Navbar from "./components/Navbar";
 import Technology from "./components/Technology";
 import Hero from './components/Hero';
+import Details from './components/Details'
 
 import "./App.css";   
+import Details from './components/Details';
 
 const App = () => {
   return (
@@ -23,6 +25,7 @@ const App = () => {
           <Route path="/technology" element={<Technology />} />
           <Route path="/fitness" element={<Fitness />} />
           <Route path="/food" element={<Food />} />
+          <Route path="/details" element={<Details/>}/>
         </Routes>
       </BrowserRouter>
     </>
